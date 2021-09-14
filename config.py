c3a57654e63c5a424d66e02000eec42d72d5b622
asadullokh1 <asadjurayev4@gmail.com>
2021-09-14 23:19:40 +0500

    Another commit for bot

--- config.py
+++ config.py
@@ -1,6 +1,2 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-BOT_TOKEN= "1862384907:AAEGcd4XrHTT4chzY1HuaS_Y679Im1cgbxc"
+BOT_TOKEN = "1862384907:AAEGcd4XrHTT4chzY1HuaS_Y679Im1cgbxc"
 
